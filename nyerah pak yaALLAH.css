23ddbc1432b22137d0f3830cc0385d70964ba8c4
cicilhub <127026305+cicilhub@users.noreply.github.com>
2023-03-05 20:10:44 +0700

    Add files via upload

---
+++ nyerah pak yaALLAH.css
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="icon" type="image/x-icon" href="assets/images/insta-favicon.ico" />
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <script src="https://kit.fontawesome.com/83ccd457ed.js" crossorigin="anonymous"></script>
+  <title>instademitgram</title>
+</head>
+
+<body>
+  <nav class="navbar">
+    <div class="navbar-wrapper">
+      <img src="assets/images/instademitgram_logo.png" alt="Instagram logo" class="logo" />
+      <input type="text" class="search" placeholder="Search" />
+      <div class="nav-items">
+        <img src="assets/images/nav-items/home.PNG" class="icons" alt="Home Icon" />
+        <img src="assets/images/nav-items/messenger.PNG" class="icons" alt="Messenger Icon" />
+        <img src="assets/images/nav-items/add.PNG" class="icons" alt="Add Icon" />
+        <img src="assets/images/nav-items/explore.PNG" class="icons" alt="Explore Icon" />
+        <img src="assets/images/nav-items/like.PNG" class="icons" alt="Like Icon" />
+        <img src="assets/images/nav-items/user.png" class="icons user-account" alt="User Profile" />
+      </div>
+    </div>
+  </nav>
+  <nav class="navbar-mobile-top">
+    <div class="navbar-wrapper">
+      <img src="assets/images/instademitgram_logo.png" alt="Instademitgram logo" class="logo" />
+      <input type="text" class="search" placeholder="Search" />
+      <div class="nav-items">
+
+        <img src="assets/images/nav-items/add.PNG" class="icons" alt="Add Icon" />
+        <img src="assets/images/nav-items/like.PNG" class="icons" alt="Like Icon" />
+        <img src="assets/images/nav-items/messenger.PNG" class="icons" alt="Messenger Icon" />
+
+      </div>
+    </div>
+  </nav>
+  <nav class="navbar-mobile-bottom">
+    <div class="navbar-wrapper">
+      <div class="nav-items">
+        <img src="assets/images/nav-items/home.PNG" class="icons" alt="Home Icon" />
+        <img src="assets/images/nav-items/send.PNG" class="icons" alt="Add Icon" />
+        <img src="assets/images/nav-items/explore.PNG" class="icons" alt="Explore Icon" />
+        <img src="assets/images/nav-items/comment.PNG" class="icons" alt="Like Icon" />
+        <img src="assets/images/nav-items/user.png" class="icons" alt="Like Icon" />
+      </div>
+    </div>
+  </nav>
+
+  <section class="main">
+    <div class="wrapper">
+      <div class="left-col">
+        <div class="status-wrapper">
+          <button class="story--scroll" id="scroll-left" onclick="scrollHorizontal(1)"><i
+              class="fas fa-chevron-left"></i></button>
+          <button class="story--scroll" id="scroll-right" onclick="scrollHorizontal(-1)"><i
+              class="fas fa-chevron-right"></i></button>
+          <div class="stories">
+            <div class="status-card">
+              <div class="status-pic">
+                <img src="assets/images/profile/status-0.png" />
+        </div>
+        <!-- Posts------------------------------------------- ---------------------- -->
+        <div class="post">
+          <div class="info">
+            <div class="user">
+              <div class="status-pic">
+                <img src="assets/images/profile/status-7.jpg" alt="Profile Image" />
+              </div>
+              <p class="username">kuntilani</p>
+            </div>
+            <img src="assets/images/nav-items/option.PNG" class="options" alt="Options" />
+          </div>
+          <img src="assets/images/post/post-1.jpg" class="post-image" alt="kuntilani post" />
+          <div class="post-content">
+            <div class="reaction-wrapper">
+              <img src="assets/images/nav-items/like.PNG" class="icons" />
+              <img src="assets/images/nav-items/comment.PNG" class="icons" />
+              <img src="assets/images/nav-items/send.PNG" class="icons" />
+              <img src="assets/images/nav-items/save.PNG" class="save icons" />
+            </div>
+            <p>Liked by <b>genderuwu</b> and <b>1,032 others</b></p>
+            <p class="description">
+              <span>kuntilani </span>menatap wajahmu
+            </p>
+            <p class="no-comments">View all 45 comments</p>
+            <p class="post-time">2 DAYS AGO</p>
+          </div>
+          <!-- <hr /> -->
+          <div class="comment-wrapper">
+            <img src="assets/images/nav-items/smile.PNG" class="icons" />
+            <input type="text" class="comment-box" placeholder="Add a comment..." />
+            <button class="comment-btn">Post</button>
+          </div>
+        </div>
+        <div class="post">
+          <div class="info">
+            <div class="user">
+              <div class="status-pic">
+                <img src="assets/images/profile/status-10.jpg" alt="Profile Image" />
+              </div>
+              <p class="username">wewegomval</p>
+            </div>
+            <img src="assets/images/nav-items/option.PNG" class="options" alt="Options" />
+          </div>
+          <img src="assets/images/post/post-2.jpg" class="post-image" />
+          <div class="post-content">
+            <div class="reaction-wrapper">
+              <img src="assets/images/nav-items/like.PNG" class="icons" />
+              <img src="assets/images/nav-items/comment.PNG" class="icons" />
+              <img src="assets/images/nav-items/send.PNG" class="icons" />
+              <img src="assets/images/nav-items/save.PNG" class="save icons" />
+            </div>
+            <p class="likes">4,316 likes</p>
+            <p class="description">
+              <span>wewegomval </span>dagang sahabat minat dm<br />
+            </p>
+            <p class="no-comments">View all 283 comments</p>
+            <p class="post-time">2 DAYS AGO</p>
+          </div>
+          <div class="comment-wrapper">
+            <img src="assets/images/nav-items/smile.PNG" class="icons" />
+            <input type="text" class="comment-box" placeholder="Add a comment..." />
+            <button class="comment-btn">Post</button>
+          </div>
+        </div>
+        <div class="post">
+          <div class="info">
+            <div class="user">
+              <div class="status-pic">
+                <img src="assets/images/profile/status-8.jpg" alt="Profile Image" />
+              </div>
+              <p class="username">pocingta</p>
+            </div>
+            <img src="assets/images/nav-items/option.PNG" class="options" alt="Options" />
+          </div>
+          <img src="assets/images/post/post-3.jpg" class="post-image" />
+          <div class="post-content">
+            <div class="reaction-wrapper">
+              <img src="assets/images/nav-items/like.PNG" class="icons" />
+              <img src="assets/images/nav-items/comment.PNG" class="icons" />
+              <img src="assets/images/nav-items/send.PNG" class="icons" />
+              <img src="assets/images/nav-items/save.PNG" class="save icons" />
+            </div>
+            <p class="likes">4,833 likes</p>
+            <p class="description">
+              <span>pocingta </span>terang kayak cintaku padamu
+            </p>
+            <p class="no-comments">View all 45 comments</p>
+            <p class="post-time">6 DAYS AGO</p>
+          </div>
+          <div class="comment-wrapper">
+            <img src="assets/images/nav-items/smile.PNG" class="icons" />
+            <input type="text" class="comment-box" placeholder="Add a comment..." />
+            <button class="comment-btn">Post</button>
+          </div>
+        </div>
+        <div class="post">
+          <div class="info">
+            <div class="user">
+              <div class="status-pic">
+                <img src="assets/images/profile/status-4.jpg" alt="Profile Image" />
+              </div>
+          <div class="action-div">
+            <button class="seeall-btn">See All</button>
+        </div>
+        <div class="profile-card">
+          <div class="status-pic">
+            <img src="assets/images/follow-img/follow-2.jpg" />
+          </div>
+            <button class="action-btn">Follow</button>
+          </div>
+        </div>
+        <div class="profile-card">
+          <div class="status-pic">
+            <img src="assets/images/follow-img/follow-4.jpg" />
+          </div>
+          <div>
+            <p class="pro-user">ramayamat</p>
+            <p class="sug-text">Suggested for you</p>
+          </div>
+          <div class="action-div">
+            <button class="action-btn">Follow</button>
+          </div>
+        </div>
+        <div class="profile-card">
+          <div class="status-pic">
+            <img src="assets/images/follow-img/follow-5.jpg" />
+          </div>
+          <div>
+            <p class="pro-user">rorokiyut</p>
+            <p class="sug-text">Suggested for you</p>
+          </div>
+          <div class="action-div">
+            <button class="action-btn">Follow</button>
+          </div>
+          
+        </div>
+        <div class="quick-links-wrapper" style="padding-top:15px;">
+          <div class="quick-link">About</div>
+          <div class="quick-link">Help</div>
+          <div class="quick-link">Press</div>
+          <div class="quick-link">API</div>
+          <div class="quick-link">Jobs</div>
+          <div class="quick-link">Privacy</div>
+          <div class="quick-link">Terms</div>
+        </div>
+        <div class="quick-links-wrapper">
+          <div class="quick-link">Locations</div>
+          <div class="quick-link">Language</div>
+        </div>
+        <div class="quick-links-wrapper" style="padding-top:10px;">
+          <div class="quick-link">@ 2022 INSTAGRAM FROM META</div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <script>
+    let currentScroll = 0;
+    let scrollAmount = 320;
+
+    const sCont = document.querySelector(".stories")
+    const hScroll = document.querySelector(".status-wrapper")
+    const leftButton = document.querySelector("#scroll-left")
+    const rightButton = document.querySelector("#scroll-right")
+
+    let maxScroll = -sCont.offsetWidth + hScroll.offsetWidth;
+    leftButton.style.opacity = '0'
+
+    function scrollHorizontal(val) {
+      currentScroll += (val * scrollAmount);
+      if (currentScroll >= 0) {
+        currentScroll = 0
+        leftButton.style.opacity = '0'
+      } else {
+        leftButton.style.opacity = '1'
+      }
+      if (currentScroll <= maxScroll) {
+        currentScroll = maxScroll
+        rightButton.style.opacity = '0'
+      } else {
+        rightButton.style.opacity = '1'
+      }
+      sCont.style.left = currentScroll + "px"
+    }
+  </script>
+</body>
+
+</html>
